72b5cde7bc5804453cb859d6b7a338a9c3052c87
Denis Dremkov <denisdremkovweb@gmail.com>
2016-03-03 11:38:38 +0200

    030316-1130

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+$(window).load(function() {
+
+	
+}); //end LOAD
+
+
+
+$(window).resize(function() {
+	var w_width = $(window).width();
+	var w_height = $(window).height();
+	var d_width = $(document).width();
+	var d_height = $(document).height();
+});
+
+
+
+
+$(document).ready(function(){
+	//ACTION - SCROLL
+	$(window).scroll(function() {
+		var top_line_h = $('.top_line').height();
+		var top_line_w = $('.top_line').outerWidth();
+		var win_scroll = $(window).scrollTop();
+		var win_width = $(window).width();
+
+})// end /Ready	
+
+
